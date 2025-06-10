503eca8d308b50d83ae692bad14de240e9f3790d
Muhammad Noshad <159698929+Muhammad-Noshad@users.noreply.github.com>
2025-06-10 20:59:21 +0500

    Update PalindromePermutationCheckerByNoshad.py
    Added solution

--- src/Ten_June/Noshad/PalindromePermutationCheckerByNoshad.py
+++ src/Ten_June/Noshad/PalindromePermutationCheckerByNoshad.py
@@ -1,73 +1,47 @@
-"""
-  Determines whether one string is a permutation of the other.
-
-  A permutation means that both strings contain the same characters
-  with the exact same frequencies, but potentially in a different order.
-
-  Args:
-    str1 (str): The first string to compare.
-    str2 (str): The second string to compare.
-
-  Returns:
-    bool: True if str1 is a permutation of str2, False otherwise.
-
-  Examples:
-    >>> is_permutation("listen", "silent")
-    True
-    >>> is_permutation("apple", "papel")
-    True
-    >>> is_permutation("rat", "car")
-    False
-    >>> is_permutation("aabbcc", "abcabc")
-    True
-"""
-
-# O(logn)
-def is_permutation_with_data_structure(str1: str, str2: str) -> bool:
-  if len(str1) != len(str2):
+# O(n)
+def is_permutation_palindrome_using_hash_map(s: str) -> bool:
+  if not s:
     return False
-  
-  dic1 = dict()
-  dic2 = dict()
-  
-  for ch in str1:
-    if ch in dic1.keys():
-      dic1[ch] += 1
-    else:
-      dic1[ch] = 1
-  
-  for ch in str2:
-    if ch in dic2.keys():
-      dic2[ch] += 1
-    else:
-      dic2[ch] = 1
-  
-  for key in dic1.keys():
-    if key not in dic2.keys():
-      return False
-    elif dic1[key] != dic2[key]:
-      return False
-  
-  return True
-
-# O(nlogn)
-def is_permutation_without_data_structure(str1: str, str2: str) -> bool:
-  if len(str1) != len(str2):
-    return False
-  
-  sorted_str1 = sorted(str1)
-  sorted_str2 = sorted(str2)
     
-  for i, ch in enumerate(sorted_str1):
-    if ch != sorted_str2[i]:
-      return False
-  
+  dic = dict()
+  for char in s.lower():
+    if char == " ":
+      continue
+    
+    if char not in dic.keys():
+      dic[char] = 0
+        
+    dic[char] += 1 
+    
+  isOddCountFound = False
+  for occurrence in dic.values():
+    if occurrence % 2 != 0:
+      if isOddCountFound:
+        return False
+      else:
+        isOddCountFound = True
+      
   return True
 
 
-str1 = "aabb"
-str2 = "aaab"
+# O(n)
+def is_permutation_palindrome_using_set(s: str) -> bool:
+  if not s:
+    return False
+  
+  odd_chars = set()
+  
+  for char in s.lower():
+    if char == " ":
+      continue
+    
+    if char in odd_chars:
+      odd_chars.remove(char)
+    else:
+      odd_chars.add(char)
+  
+  return len(odd_chars) <= 1
 
 
-print(is_permutation_with_data_structure(str1, str2))
-print(is_permutation_without_data_structure(str1, str2))
+print(is_permutation_palindrome_using_hash_map("tacdac"))
+print(is_permutation_palindrome_using_set("tacdac"))
